58d93127851993adfda78018cadbb410c73631ac
ANG GEOK EN <20047223@myrp.edu.sg>
2021-04-29 16:20:11 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20047223/l02_registrationform/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20047223.l02_registrationform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.excerised2);
+    }
+}
